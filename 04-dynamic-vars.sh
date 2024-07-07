eaef04e06d910e7917c7f9c10b22d701dcabb649
Saibalaji0309 <jallipallisai621@gmail.com>
2024-07-07 18:43:38 +0530

    color changing

--- 04-dynamic-vars.sh
+++ 04-dynamic-vars.sh
@@ -7,6 +7,6 @@ echo "Todays date is $DATE"
 # checking how many active server are running
 act=$(who | wc -l)
 
-echo "Total active server $act"
+echo -e "Total active server $act"
 
-echo "Total active server \e[32m $act \e[0m"
+echo -e "Total active server \e[32m $act \e[0m"
