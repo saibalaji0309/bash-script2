39576e15dbc40256b68bec05dbf7608f7062bdc3
Saibalaji0309 <jallipallisai621@gmail.com>
2024-07-07 22:19:02 +0530

    special variable changing mawa

---
+++ 06-exitcodes.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# ecah and ever action in linux have a exit code
+# 0 to 255 id the range of the codes
+# among all, 0 representaction completed sucessfull .
+# anything in b.\w 1 to 255 represent either parti
