b5ba4297c919498f3140251dea3e90b9a7ce0760
Saibalaji0309 <jallipallisai621@gmail.com>
2024-07-07 17:12:53 +0530

    color changing

--- sample2-color-code.sh
+++ sample2-color-code.sh
@@ -12,4 +12,8 @@
     # Magenta     35              45
     # Cyan        36              46
 
-echo -e '\e[32m this text color in red [0m'
+echo -e '\e[32m this text color in green [0m'
+echo -e '\e[34m mawa ee text color chappu [0m'
+
+# if you want to check the background color too:
+echo -e '\e[44m 36m this return the background as blue and text color as cyan [0m'
