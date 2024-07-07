d3c45a8d7f3491880bfa58eb8fa6553fae3cac6e
Saibalaji0309 <jallipallisai621@gmail.com>
2024-07-07 16:30:46 +0530

    checking alias

--- sample.sh
+++ sample.sh
@@ -14,4 +14,7 @@ echo checking the alias value gp wher in vs we make git add,....git push and the
 # sai
 # Balaji
 
-echo -e sai\nBalaji
+echo -e sai\nBalaji #--this doesn't return in new line because
+# here '-e' stands for enable the escape character
+# the error is be we didn't put the characters in quotation's
+echo -e 'sai\nBalaji'
