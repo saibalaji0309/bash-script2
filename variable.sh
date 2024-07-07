0c7024708d4c55f75ab8436b6d5d8905c7459447
Saibalaji0309 <jallipallisai621@gmail.com>
2024-07-07 17:21:05 +0530

    color changing

---
+++ variable.sh
@@ -0,0 +1,21 @@
+# !/bin/bash
+
+# what is variable? 
+# it's generally use to hold the dybamical values
+
+a=10
+b=33
+x=afsd
+
+# in bash ther is no data types 
+
+echo a
+echo $a
+echo 'print the value of a $a'
+
+# did find the difference b/w above two satatements 
+# 1--> give : a
+# 2--> give value of a : 10
+
+# q)  when to try to print the value that does not defined before what will happen?
+echo 'print the value of mama: $mama'
