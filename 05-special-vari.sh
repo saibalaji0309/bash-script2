3bed305d972ea82d005fa7e1406d7d04af8128c5
Saibalaji0309 <jallipallisai621@gmail.com>
2024-07-07 19:09:26 +0530

    special variable changing mawa

--- 05-special-vari.sh
+++ 05-special-vari.sh
@@ -13,3 +13,8 @@ echo "name of the newly launched ev-bike : $1"
 echo "What is treding topic on instgram : $2"
 # this $1 and $2 are the argumnets you can pass after the file name like
 # bash 05-special-vari.sh Ntorch "Prabhash sign a new movie"
+
+
+Q) i want arguments after 10 also?
+# bash scriptname.sh arg1 arg2 arg3 arg4 arg5 arg6 arg7 arg8 arg9 arg10........
+# at arg10 --> this is considered as a arg1 --> this is know as paddiling
