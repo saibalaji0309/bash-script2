05ab7b7efafc3172ec971f73c6453f1ece0c31e9
Saibalaji0309 <jallipallisai621@gmail.com>
2024-07-16 17:34:11 +0530

    special variable changing mawa

---
+++ 08-commands.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# there are 4 types of commands
+# they are :
+            # 1) Binary       --(/bin  ,/sbin) bin commands are regulary commands .sbin securebin this are invoked by root user
+            # 2) Alias        --(alias are shortcut)
+            # 3) shell Built-in commands  -- -- in shell type this -- type cd, type echo,type break 
+            # 4) Functions   -- are the group of commands that which are wriiten in your choicce u can caal them when ever u want
+
+# how to declear the fuction
+
+# f(){
+#     echo hai
+# }
+
+# f
+
