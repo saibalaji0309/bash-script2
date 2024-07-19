af4924ff233cd0f17a16a7f3eefb6cd160962d83
Saibalaji0309 <jallipallisai621@gmail.com>
2024-07-19 18:56:35 +0530

    malli update chasa

--- 12-loop.sh
+++ 12-loop.sh
@@ -5,6 +5,6 @@
 # 1) for loops  #if u  know how many times to repeat the loop
 # 2) while loops  #if u don't know how many time to repeat
 
-for val in 10 20 30 40; do
+for val in 10 20 30 40 ; do
     echo "values in the loop if : $var"
 done
